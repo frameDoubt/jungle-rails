c428f6024230814acf478578c576a30e3f3e8d3b
Brandon Wright <brandon.w.wright4@gmail.com>
2021-06-01 23:11:26 +0000

    add template fixes for categories/new and methods to categories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,21 @@
 class Admin::CategoriesController < ApplicationController
   def index
     @categories = Category.all.order(:name)
+    # raise @categories.inspect
   end
   def new
     @category = Category.new
   end
   def create
+    @category = Category.new(category_params)
   end
+
+  private
+
+  def product_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
 end
