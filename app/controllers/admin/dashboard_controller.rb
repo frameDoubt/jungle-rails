a2d2fee06cc753d644486e4a367e3b91b702bf56
Brandon Wright <brandon.w.wright4@gmail.com>
2021-06-09 17:33:47 +0000

    add @number_of_products variable to admin/dashboard_controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"], except: false
   def show
     @number_of_categories = Category.count(:id)
-    puts @number_of_categories
+    @number_of_products = Product.sum(:quantity)
+    puts @number_of_products
   end
 end
