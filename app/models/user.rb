433675c971a7626865cc78b9e2a8f512bbba36e7
Brandon Wright <brandon.w.wright4@gmail.com>
2021-06-15 19:36:21 +0000

    add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
   has_secure_password
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,26 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validation' do
+    before :each do
+      @user = User.new(
+        first_name: 'User',
+        last_name: 'One',
+        email: 'emailOne@email.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+    end
+                
     it "is valid" do
-      user = User.new(
-                    first_name: 'User_One_Computer',
-                    last_name: 1_234.75,
-                    email: 10,
-                    password: 'password',
-                    password_confirmation: 'password'
-                  )
-      expect(product).to be_valid
+      expect(@user).to be_valid
+    end
+    it "is not valid without a password" do
+      @user.password = nil
+      expect(@user).to_not be_valid
+    end
+    it "is not valid without a password_confirmation" do
+      @user.password_confirmation = nil
+      expect(@user).to_not be_valid
     end
   end
 end
