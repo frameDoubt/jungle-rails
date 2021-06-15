f8ea8a0c946cc46d4b6391213f932e25f5892e47
Brandon Wright <brandon.w.wright4@gmail.com>
2021-06-15 19:04:40 +0000

    add validation test for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validation' do
+    it "is valid" do
+      user = User.new(
+                    first_name: 'User_One_Computer',
+                    last_name: 1_234.75,
+                    email: 10,
+                    password: 'password',
+                    password_confirmation: 'password'
+                  )
+      expect(product).to be_valid
+    end
+  end
 end
