ac585c9a65aec1ed09ac5bff69cc03f577ea085c
Brandon Wright <brandon.w.wright4@gmail.com>
2021-06-16 17:05:25 +0000

    add test for product information

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see product information, like details" do
+    # ACT
+    visit root_path
+
+    # DEBUG / VERIFY
+    save_screenshot
+    expect(page).to have_css 'article.product', count: 10
+  end
+end
